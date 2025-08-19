1d9527ce6a3501b09071fbaa231385a144a5c541
YAĞMUR <yagmur.kolanc@gmail.com>
2025-08-19 10:00:32 +0300

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class User(AbstractUser):
-    ROLE_CHOICES = (
-        ('teacher', 'Teacher'),
-        ('parent', 'Parent'),
-    )
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
